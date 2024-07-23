3fbc05810aa135e07dd3940d9cbf6c58668d0aa0
baha <baktiarzaksylykov22@gmail.com>
2024-07-23 08:22:07 -0400

    initial commit

---
+++ router.py
@@ -0,0 +1,22 @@
+from fastapi import Depends
+
+from fastapi import APIRouter
+from repository import TaskRepository
+from schemas import STask, STaskAdd, STaskId
+
+router = APIRouter(
+    prefix="/tasks",
+    tags=["Таски"],
+)
+
+
+@router.post("")
+async def add_task(task: STaskAdd = Depends()) -> dict[str, int]:
+    new_task_id = await TaskRepository.add_task(task)
+    return {"id": new_task_id}
+
+
+@router.get("")
+async def get_tasks() -> list[STask]:
+    tasks = await TaskRepository.get_tasks()
+    return tasks
